26621da3d7e69c44ea5342d8dd409641a530642d
yevgeller <yevgeller@gmail.com>
2022-02-23 19:24:11 -0600

    Abstract factory is kind of done

---
+++ DesignPatternsRepo1/Pages/Creational/Abstract-solution.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages.Creational
+{
+    public class Abstract_solutionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
