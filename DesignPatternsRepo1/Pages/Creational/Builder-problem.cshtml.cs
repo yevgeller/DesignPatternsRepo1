f40a7a6754acbb2071125566109ee89b95721102
yevgeller <yevgeller@gmail.com>
2022-02-26 23:36:28 -0600

    Work on builder, overall site appearance

---
+++ DesignPatternsRepo1/Pages/Creational/Builder-problem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages.Creational
+{
+    public class Builder_problemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
