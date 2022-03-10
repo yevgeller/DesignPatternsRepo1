2f0dc434e356f865c914cc2d04d9271299aecaef
yevgeller <yevgeller@gmail.com>
2022-03-09 18:27:43 -0600

    Page for Adapter, new folder for Structural patterns

---
+++ DesignPatternsRepo1/Pages/Structural/Adapter-problem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages.Structural
+{
+    public class Adapter_problemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
