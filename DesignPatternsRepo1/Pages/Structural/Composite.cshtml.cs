351a7ac548099a1b51b3abfbc7efbcc23128eae3
yevgeller <yevgeller@gmail.com>
2022-03-25 20:16:00 -0500

    Page for Composite

---
+++ DesignPatternsRepo1/Pages/Structural/Composite.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages.Structural
+{
+    public class CompositeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
