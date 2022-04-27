adb8b776539be800236acbde91043509c69b700b
yevgeller <yevgeller@gmail.com>
2022-04-27 11:37:56 -0500

    More links updates

---
+++ DesignPatternsRepo1/Pages/Structural/Proxy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages.Structural
+{
+    public class ProxyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
