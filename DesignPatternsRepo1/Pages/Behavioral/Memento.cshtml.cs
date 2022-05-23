fe7b3759aa211d3de595921903c57c5129bf41e6
yevgeller <yevgeller@gmail.com>
2022-05-23 08:14:36 -0500

    Memento

---
+++ DesignPatternsRepo1/Pages/Behavioral/Memento.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages.Behavioral
+{
+    public class MementoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
