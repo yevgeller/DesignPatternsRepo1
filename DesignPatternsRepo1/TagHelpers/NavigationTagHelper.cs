8dd33896333a5f3f6623bfd00f746f326573f73b
DEV03\EGEL23 <egeller@loadspring.com>
2022-06-14 21:17:31 -0500

    Working on Navigation tag helper

--- DesignPatternsRepo1/TagHelpers/NavigationTagHelper.cs
+++ DesignPatternsRepo1/TagHelpers/NavigationTagHelper.cs
@@ -1,6 +1,13 @@
-﻿namespace DesignPatternsRepo1.TagHelpers
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DesignPatternsRepo1.TagHelpers
 {
-    public class NavigationTagHelper
+    public class NavigationTagHelper : TagHelper 
     {
+        public string Pattern { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
     }
 }
