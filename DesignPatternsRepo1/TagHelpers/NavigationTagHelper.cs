3752611b80a4ff633908873a52379125a3bdc860
yevgeller <yevgeller@gmail.com>
2022-06-16 08:36:47 -0500

    nav tag helper work

--- DesignPatternsRepo1/TagHelpers/NavigationTagHelper.cs
+++ DesignPatternsRepo1/TagHelpers/NavigationTagHelper.cs
@@ -4,12 +4,27 @@ namespace DesignPatternsRepo1.TagHelpers
 {
     public class NavigationTagHelper : TagHelper
     {
-        public string Pattern { get; set; }
+        private string _pattern = "factory";
+        public string Pattern { get { return _pattern; } set { this._pattern = value.ToLower(); } }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "div";
             output.Attributes.SetAttribute("class", "container is-flex is-flex-direction-row is-justify-content-space-evenly mt-6 ");
-            output.Content.Append(this.Pattern);
+            output.Content.AppendHtml($@"
+<div>
+            <span class='is-uppercase has-text-weight-semibold'>Creational</span>
+            <div class='is-flex is-flex-direction-column is-justify-content-space-evenly'>");
+            if (this.Pattern == "factory") output.Content.AppendHtml("<div><i class='fa-solid fa-industry mr-2'></i></div>");
+            else output.Content.AppendHtml("<div><a asp-page='/Creational/Factory-problem'><i class='fa-solid fa-industry mr-2'></i>Factory</a></div>");
+            //                <div><a asp-page='/Creational/Abstract-problem'><i class='fa-solid fa-cube'></i><i class='fa-solid fa-industry mr-2'></i>Abstract Factory</a></div>
+            //                <div><a asp-page='/Creational/Builder-problem'><i class='fa-solid fa-helmet-safety mr-2'></i>Builder</a></div>
+            //                <div><a asp-page='/Creational/Prototype'><i class='fa-regular fa-clone mr-2'></i>&nbsp;Prototype</a></div>
+            //                <div><a asp-page='/Creational/Singleton'><i class='fa-solid fa-atom mr-2'></i>&nbsp;Singleton</a></div>
+
+            //            </div>
+            //        </div>
+            //");
+            output.Content.AppendHtml("</div>");
         }
     }
 }
