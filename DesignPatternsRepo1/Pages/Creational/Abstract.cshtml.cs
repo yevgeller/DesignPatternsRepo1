4c88d50399c665839c67f5590cb0fd49f2a9718c
yevgeller <yevgeller@gmail.com>
2022-06-22 16:02:46 -0500

    Redoing Abstract Factory

--- DesignPatternsRepo1/Pages/Creational/Abstract.cshtml.cs
+++ DesignPatternsRepo1/Pages/Creational/Abstract.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DesignPatternsRepo1.Pages.Creational
 {
-    public class AbstractModel1 : PageModel
+    public class AbstractModel : PageModel
     {
         public void OnGet()
         {
