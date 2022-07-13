98557e2c93977e982108f13e01a9d4e4a0cd3da6
yevgeller <yevgeller@gmail.com>
2022-07-13 11:38:46 -0500

    Breadcrumb interim commit

--- DesignPatternsRepo1/TagHelpers/BreadcrumbTagHelper.cs
+++ DesignPatternsRepo1/TagHelpers/BreadcrumbTagHelper.cs
@@ -1,6 +1,15 @@
-﻿namespace DesignPatternsRepo1.TagHelpers
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DesignPatternsRepo1.TagHelpers
 {
-    public class BreadcrumbTagHelper
+    public class BreadcrumbTagHelper : TagHelper
     {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "container is-flex is-flex-direction-row is-justify-content-space-evenly mt-6 ");
+            output.Content.AppendHtml($@"<div><span class='is-uppercase has-text-weight-semibold ml-5'>Home</span><div class='is-flex is-flex-direction-column is-justify-content-space-evenly'><div><a href='/'><i class='fa-solid fa-house-user mr-2 ml-5'></i>Home</a></div></div></div>");
+
+        }
     }
 }
