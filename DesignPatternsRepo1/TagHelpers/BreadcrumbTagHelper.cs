2b82d7027d1f5dcfa1aa2448a0a108e6f768f970
yevgeller <yevgeller@gmail.com>
2022-07-14 21:23:43 -0500

    Breadcrumb tag helper update

--- DesignPatternsRepo1/TagHelpers/BreadcrumbTagHelper.cs
+++ DesignPatternsRepo1/TagHelpers/BreadcrumbTagHelper.cs
@@ -12,11 +12,17 @@ namespace DesignPatternsRepo1.TagHelpers
   </ul>
 </nav>
         */
+
+        public string Group { get; set; } = "Behavioral";
+        public string PageName { get; set; } = "Factory";
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "nav";
-            output.Attributes.SetAttribute("class", "container is-flex is-flex-direction-row is-justify-content-space-evenly mt-6 ");
-            output.Content.AppendHtml($@"<div><span class='is-uppercase has-text-weight-semibold ml-5'>Home</span><div class='is-flex is-flex-direction-column is-justify-content-space-evenly'><div><a href='/'><i class='fa-solid fa-house-user mr-2 ml-5'></i>Home</a></div></div></div>");
+            output.Attributes.SetAttribute("class", "breadcrumb");
+            output.Attributes.SetAttribute("aria-label", "breadcrumbs");
+
+            output.Content.AppendHtml($@"<ul><li><a href='/'>{Group}</a></li><li class='is-active'><a href = '#' aria-current='page'>{PageName}</a></li></ul>");
 
         }
     }
