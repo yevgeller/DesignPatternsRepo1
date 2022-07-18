a54b22e2c8c43a4203abb8f30dd40f540e0ad77e
yevgeller <yevgeller@gmail.com>
2022-07-18 11:39:51 -0500

    SDP model

---
+++ DesignPatternsRepo1/Models/SoftwareDesignPattern.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPatternsRepo1.Models
+{
+    public class SoftwareDesignPattern
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public string Summary { get; set; }
+        public string Icon { get; set; }
+
+        public SoftwareDesignPattern()
+        {
+            this.Name = "No name";
+            this.Group = "No group";
+            this.Summary = "Pattern summary";
+            this.Icon = "fa-folder-open";
+        }
+
+        public SoftwareDesignPattern(string code)
+        {
+            switch (code.ToLower())
+            {
+                case "fac":
+                    this.Name = "Factory";
+                    this.Group = "Creational";
+                    this.Summary = "Creation of objects that conform to the same interface without specifying concrete classes";
+                    this.Icon = "fa-industry";
+                    break;
+                default:
+                    this.Name = "Abstract Factory";
+                    this.Group = "Creational";
+                    this.Summary = "Same as Factory (Creation of objects that conform to the same interface without specifying concrete classes) but to 'families' of objects";
+                    this.Icon = "fa-boxes-stacked";
+                    break;
+            }
+        }
+
+    }
+}
