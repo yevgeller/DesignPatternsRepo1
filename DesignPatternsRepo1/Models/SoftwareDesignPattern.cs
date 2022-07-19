171567ccf0748160c6e92967b1d4e1e2b2d54d1f
yevgeller <yevgeller@gmail.com>
2022-07-19 09:46:59 -0500

    Update to SDP class

--- DesignPatternsRepo1/Models/SoftwareDesignPattern.cs
+++ DesignPatternsRepo1/Models/SoftwareDesignPattern.cs
@@ -25,6 +25,32 @@
                     this.Summary = "Creation of objects that conform to the same interface without specifying concrete classes";
                     this.Icon = "fa-industry";
                     break;
+                case "abs":
+                    this.Name = "Abstract Factory";
+                    this.Group = "Creational";
+                    this.Summary = "Same as Factory (Creation of objects that conform to the same interface without specifying concrete classes) but to 'families' of objects";
+                    this.Icon = "fa-boxes-stacked";
+                    break;
+                case "bui": //Builder
+                case "proto": //Prototype
+                case "sin": //Singleton
+                case "ada": //Adapter
+                case "bri": //Bridge
+                case "sit": //Composite
+                case "dec": //Decorator
+                case "fas": //Facade
+                case "fly": //Flyweight
+                case "proxy": //Proxy
+                case "cha": //Chain of Responsibility
+                case "com": //Command
+                case "int": //Interpreter
+                case "ite": //Iterator
+                case "med": //Mediator
+                case "obs": //Observer
+                case "sta": //State
+                case "str": //Strategy
+                case "tem": //Template Method
+                case "vis": //Visitor
                 default:
                     this.Name = "Abstract Factory";
                     this.Group = "Creational";
