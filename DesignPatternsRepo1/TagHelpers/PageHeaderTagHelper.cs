55dd3bccf10f0830a4eb86d16a9bf5306b00446d
yevgeller <yevgeller@gmail.com>
2022-07-21 12:31:50 -0500

    PageHeader tag helper

---
+++ DesignPatternsRepo1/TagHelpers/PageHeaderTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatternsRepo1.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DesignPatternsRepo1.TagHelpers
+{
+    public class PageHeaderTagHelper : TagHelper
+    {
+        /*
+         * <h1 class="title is-1 is-spaced">
+                <i class="fa-solid fa-boxes-stacked mr-2"></i>
+                Abstract Factory
+            </h1>
+            <h2 class="subtitle">Organization ... classes</h2>
+        */
+        public SoftwareDesignPattern SoftwareDesignPattern { get; set; } = new SoftwareDesignPattern();
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml($@"<h1 class='title is-1 is-spaced'><i class='fa-solid {SoftwareDesignPattern.Icon} mr-2'></i>");
+            output.Content.AppendHtml($@"{SoftwareDesignPattern.Name}</h1>");
+            output.Content.AppendHtml($@"<h2 class='subtitle'>{SoftwareDesignPattern.Summary}</h2>");
+        }
+    }
+}
