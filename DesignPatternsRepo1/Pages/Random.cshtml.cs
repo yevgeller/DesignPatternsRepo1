01e3332fabf8b53ac374a7db3ef7efa55f16fb7f
yevgeller <yevgeller@gmail.com>
2022-08-07 21:12:59 -0500

    Random pattern page

---
+++ DesignPatternsRepo1/Pages/Random.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages
+{
+    public class RandomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
