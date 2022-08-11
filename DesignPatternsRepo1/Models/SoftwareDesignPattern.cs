5f038fdf0b0c022071bad595a31bfaf4a855861e
yevgeller <yevgeller@gmail.com>
2022-08-11 18:12:37 -0500

    db update

--- DesignPatternsRepo1/Models/SoftwareDesignPattern.cs
+++ DesignPatternsRepo1/Models/SoftwareDesignPattern.cs
@@ -161,7 +161,10 @@
                     this.InformalDescription = "A set up for series of checks on an item. Checks must happen sequentially, but not necessarily from the beginning.";
                     this.RecipeSteps = new List<string>
                     {
-
+                        "Set up a special interface for steps needing to be performed",
+                        "Flesh out concrete steps",
+                        "Connect steps in a chain",
+                        "Work an item through the chain"
                     };
                     this.GroupEnum = PatternGroup.Behavioral;
                     break;
