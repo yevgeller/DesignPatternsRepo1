d133e4ebc9281d9fb4d21af36e9561e7db2f02b7
yevgeller <yevgeller@gmail.com>
2022-09-09 23:25:41 -0400

    Testing something

---
+++ DesignPatternsRepo1/Pages/Test1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesignPatternsRepo1.Pages
+{
+    public class Test1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
