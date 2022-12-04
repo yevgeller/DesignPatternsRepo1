97dd2b6755d2b5701adf6b9b9b9c9c31316f201f
yevgeller <yevgeller@gmail.com>
2022-12-03 20:12:52 -0500

    Updating tag helpers

--- DesignPatternsRepo1/TagHelpers/NewBadgeTagHelper.cs
+++ DesignPatternsRepo1/TagHelpers/NewBadgeTagHelper.cs
@@ -9,12 +9,7 @@ namespace DesignPatternsRepo1.TagHelpers
         {
             output.TagName = "span";
             output.Attributes.SetAttribute("class", "is-size-7 has-text-primary is-uppercase mr-3");
-
-            //var icon = new TagBuilder("i");
-            //icon.Attributes.Add("class", "fa-regular fa-square-plus mr-1");
-            //output.Content.AppendHtml(icon);
-
-            output.Content.AppendHtml("<span class='ml-1'>new</span>");
+            output.Content.AppendHtml("<span class='ml-1'>.</span>");
         }
     }
 }
