95ce07fc94306c9fb03722718360bbde9fa29e7e
yevgeller <yevgeller@gmail.com>
2022-12-17 22:29:05 -0500

    Interim commit

--- DesignPatternsRepo1/wwwroot/js/minesGame.js
+++ DesignPatternsRepo1/wwwroot/js/minesGame.js
@@ -648,7 +648,7 @@ function showResults() {
     results.forEach(x => {
         debugger;
         let candidate = headers.filter(h =>
-            (h.headerComparator == x.headerObject.comparator && h.headerComparator == number.MIN_SAFE_INTEGER)
+            (h.header == x.headerObject.header && h.headerComparator == number.MIN_SAFE_INTEGER)
             ||
             (h.header == x.headerObject.header
             && h.headerComparator == x.headerObject.headerComparator
